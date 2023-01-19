0ec12c4995440c155317355e1211bad0bb77391f
kowordidi <891olivermoran@gmail.com>
2023-01-19 21:59:18 +0100

    file manager added

---
+++ src/man_file.py
@@ -0,0 +1,19 @@
+import os
+
+def dir_path_root() -> str:
+    """ root dir (abs) path of project - contains 'src', 'test', 'prj' ... """
+    dir_path_parent = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    return dir_path_parent
+
+
+
+def read_file(file_name):
+    file_path = os.path.join(dir_path_root(), file_name)
+    print(f"....trying to read file: {file_path}")
+    with open(file_path, encoding="utf-8") as f:
+        cts = f.read()
+        print(f"file contemts....{lines}")
+
+# ---------------------------------------
+if __name__ == "__main__":
+    read_file("hey.txt")
