ffdc9ac5982e8ce78802411b1ee7347a8a02d571
kowordidi <891olivermoran@gmail.com>
2023-01-19 21:39:16 +0100

    refactor - inner function for idx_of_closest_line

--- src/grid.py
+++ src/grid.py
@@ -12,13 +12,16 @@ def distance(p, g):
 
 
 def idx_of_closest_grid_line(p, grid):
-    shortest_distance = None
+    shortest_dist = None
     ret = None
     for grid_idx in range(len(grid)):
         dist = distance(p, grid[grid_idx])
+        
+        def is_shorter(shortest_so_far, curr):
+            return shortest_so_far is None or curr < shortest_so_far
 
-        if shortest_distance is None or dist < shortest_distance:
-            shortest_distance = dist
+        if is_shorter(shortest_dist, dist):
+            shortest_dist = dist
             ret = grid_idx
 
     return ret
