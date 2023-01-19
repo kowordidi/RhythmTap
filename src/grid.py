06d5000c013249399527020335262747c98f86c2
kowordidi <891olivermoran@gmail.com>
2023-01-19 20:09:15 +0100

    added new file for functions

---
+++ src/grid.py
@@ -0,0 +1,27 @@
+def snap(points, grid):
+    snapped = []
+
+    for p in points:
+        snapped.append(closest_grid_line(p, grid))
+
+    return snapped
+
+
+
+
+
+def distance(p, g):
+    return abs(p - g)
+
+
+
+def closest_grid_line(p, grid):
+    shortestDistance = 100
+    positionOfClosestGridLine = None
+    for g in range(len(grid)):
+
+        if distance(p, g) < shortestDistance:
+            shortestDistance = distance(p, g)
+            positionOfClosestGridLine = g
+
+    return positionOfClosestGridLine
