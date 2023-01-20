dd1aeb4104b11e8f5de9fb674650fc0f6a952ed6
kowordidi <891olivermoran@gmail.com>
2023-01-20 11:48:44 +0100

    the tests aren't working and I don't know why. It just says there are none

--- src/main.py
+++ src/main.py
@@ -1,16 +1,13 @@
 from src.grid import snap
 
 
-
 def my_main():
     # 4/4 time, with one bar, spanning 4 seconds
     a_list_of_points = [0.8, 2, 3.1]
     a_grid = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]
 
-
     print("expected result: [1, 2, 3]")
     print("result:", snap(a_list_of_points, a_grid))
-
 
 
 # ---------------------------------------
--- tst/test_grid.py
+++ tst/test_grid.py
@@ -1,11 +1,13 @@
-from src.grid import distance, idx_of_closest_grid_line
+from src.grid import distance, idx_of_closest_grid_line, snap
 
 
+# ---------------------------------------
 def test_distance():
     assert distance(4, 5) == 1
     assert distance(5, 4) == 1
 
 
+# ---------------------------------------
 def test_closest_grid_line():
     aGrid = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]
     assert idx_of_closest_grid_line(0.3, aGrid) == 1
@@ -14,6 +16,12 @@ def test_closest_grid_line():
 
 
 # ---------------------------------------
+def test_snap():
+    assert snap([0.3, 4.1, 5], [0, 0.5, 4, 5]) == [0.5, 4, 5]
+
+
+# ---------------------------------------
 if __name__ == "__main__":
     test_distance()
-
+    test_closest_grid_line()
+    test_snap()
