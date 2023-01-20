967e8a9b7248e22237cf8a1dc847065e63793123
kowordidi <891olivermoran@gmail.com>
2023-01-20 21:24:00 +0100

    dunno

--- readme.md
+++ readme.md
@@ -3,12 +3,12 @@ This readme is formatted with [markdown](https://docs.github.com/en/get-started/
 
 # RhythmTap
 
-##  Oliver's cool project for analyzing and improving rhythms :-)
+
 
 
 ### todo
+* write a function that generates specific grids
 * tests for man_file
-* test for main function - snap()
 * read actual grid and sample pts from the files
 * guess what - tests for that then too :-)
 
--- src/man_file.py
+++ src/man_file.py
@@ -1,19 +1,22 @@
 import os
 
+
+# ---------------------------------------
 def dir_path_root() -> str:
     """ root dir (abs) path of project - contains 'src', 'test', 'prj' ... """
     dir_path_parent = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     return dir_path_parent
 
 
-
+# ---------------------------------------
 def read_file(file_name):
     file_path = os.path.join(dir_path_root(), "dat", file_name)
     print(f"....trying to read file: {file_path}")
     with open(file_path, encoding="utf-8") as f:
         cts = f.read()
-        print(f"file contemts....{cts}")
+        print(f"file contents....{cts}")
+
 
 # ---------------------------------------
 if __name__ == "__main__":
-    read_file("hey.txt")
+    read_file("eight_grid.txt")
