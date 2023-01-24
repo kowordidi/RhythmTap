ea8787c607bb52506f5c30ac2754f50bed92ad23
kowordidi <891olivermoran@gmail.com>
2023-01-24 23:40:14 +0100

    it fucking works, ye cunt

--- src/functions.py
+++ src/functions.py
@@ -5,28 +5,14 @@ CONST_NUMBER_OF_BEATS = 4
 
 
 # ---------------------------------------
-# def snap(points):
-#     snapped = []
-#     unsnapped_points = points
-#     eighth_grid = generate_grid(2, CONST_NUMBER_OF_BEATS)
-#     triplet_grid = generate_grid(3, CONST_NUMBER_OF_BEATS)
-#
-#     def find_next_snapped(grid, p):
-#         return grid[idx_of_closest_grid_line(p, grid)]
-#
-#     for p in points:
-#         next_snapped = find_next_snapped(eighth_grid, p)
-#         if distance(next_snapped, p) <= CONST_THRESHOLD:
-#             snapped.append(next_snapped)
-#             unsnapped_points.pop(p)
-#
-#     if len(unsnapped_points) > 0:
-#         next_snapped = find_next_snapped(triplet_grid, p)
-#         if distance(next_snapped, p) <= CONST_THRESHOLD:
-#             snapped.append(next_snapped)
-#             unsnapped_points.pop(p)
-#
-#     return snapped
+
+def snap(points):
+    snapped = []
+    grid = generate_grid(CONST_NUMBER_OF_BEATS)
+    for p in points:
+        next_snapped = grid[idx_of_closest_grid_line(p, grid)]
+        snapped.append(next_snapped)
+    return snapped
 
 
 # ---------------------------------------
--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
-from src.functions import read_inputs, generate_grid
+from src.functions import read_inputs, snap, generate_grid
 from src.man_file import read_file
 
 
 def my_main():
-    print(generate_grid(4))
+    points = read_inputs()
+    res = snap(points)
+    print(f'this is the input received: {points}')
+    print('this is the expected result: [0, 0.3333, 0.6667, 1.0, 1.5, 2.0, 3.0]')
+    print(f'this is the result:{res}')
+
+
 # ---------------------------------------
 if __name__ == "__main__":
     my_main()
