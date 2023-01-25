6c4c5e7ca812ae2129da41b81aea84ea8428bf04
kowordidi <891olivermoran@gmail.com>
2023-01-25 11:53:55 +0100

    I manged to have musescore files be created. They are still wrong but I'll figure out why later

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src.snap_functions import read_inputs, snap, generate_grid, CONST_NUMBER_OF_BEATS
-from src.note_value_functions import name_list
+from src.note_value_functions import name_list, generate_notes
+from music21 import *
 
 
 
@@ -12,7 +13,8 @@ def my_main():
     print('this is the expected result: [0, 0.3333, 0.6667, 1.0, 1.5, 2.0, 3.0]')
     print(f'this is the result:{res}')
 
-    print(name_list(res))
+    generate_notes(res)
+
 
 
 # ---------------------------------------
--- src/note_value_functions.py
+++ src/note_value_functions.py
@@ -5,22 +5,7 @@ from music21 import note, stream
 
 
 def calculate_note_value(starting_point, ending_point):
-    note_length = distance(starting_point, ending_point)
-    note_value = None
-    if note_length == 4.0:
-        note_value = 'whole'
-    if note_length == 2.0:
-        note_value = 'half'
-    if note_length == 1.0:
-        note_value = 'quarter'
-    if note_length == 0.5:
-        note_value = 'eighth'
-    if note_length == 0.25:
-        note_value = 'sixteenth'
-    if note_length == 0.125:
-        note_value = 'thirty-second'
-    if round(note_length, 2) == 0.33:  # I might have to import decimal because python has arithmetic error here
-        note_value = 'triplet'
+    note_value = distance(starting_point, ending_point)
     return note_value
 
 
@@ -38,6 +23,13 @@ def name_list(snapped_points):
     return note_values
 
 
-# def generate_notes(note_values):
-#     def generate_note(note_value):
-#         n = note.Note('C4', )
+def generate_notes(note_values):
+    def generate_note(note_value):
+        n = note.Note('C4', quarterLength=note_value)
+        return n
+
+    s = stream.Stream()
+    for note_value in note_values:
+        s.append(generate_note(note_value))
+    s.show()
+    return s
